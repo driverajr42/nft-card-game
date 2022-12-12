1397387506decd88ad1a61339511a2e401deeb67
Dennis <106558555+driverajr42@users.noreply.github.com>
2022-12-12 14:46:57 -0500

    create a page High Order Component to wrap all components with.

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { PageHOC  } from '../components';
+
+const Home = () => {
+  return (
+    <div>
+     
+    </div>
+  )
+};
+
+export default PageHOC (
+  Home, 
+  <>Welcome to Avax Gods <br /> a Web3 NFT Game</>,
+  <>Connect your waller to start playing <br /> the ultimate Web3 battle Card Game</>
+);
